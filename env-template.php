b0e9318a7dcf5bf12c1f42c73cb261df7db21ed1
dgcollier <david@gcollier.me>
2015-11-05 17:46:05 -0600

    adding project

---
+++ env-template.php
@@ -0,0 +1,13 @@
+<?php
+	
+	return [
+		'DB_HOST' => '$_ENV['DB_HOST']',
+		'DB_NAME' => '$_ENV['DB_NAME']',
+		'DB_USER' => '$_ENV['DB_USER']',
+		'DB_PASS' => '$_ENV['DB_PASS']',
+
+		'USER_EMAIL'     => '$_ENV['USER_EMAIL']',
+		'USER_USERNAME'  => '$_ENV['USER_USERNAME']',
+		'USER_PASSWORD'  => '$_ENV['USER_PASSWORD']',
+
+	];
